f9db918b880bc71982490162fe921e62166a0c83
Wei Zhang <iusenenow@gmail.com>
2021-02-09 09:35:54 +1100

    backend update

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -64,10 +64,4 @@ Rating.defaultProps = {
   color: '#f8e825'
 }
 
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string.isRequired,
-}
-
 export default Rating
